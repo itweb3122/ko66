865e48f57eec762d7fc4102e2c67d950d9d9bb04
itweb3122 <itweb3122@gmail.com>
2024-07-15 14:23:39 +0700

    add whitenoise

--- ko66new/settings.py
+++ ko66new/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # third part
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ko66new.urls'
